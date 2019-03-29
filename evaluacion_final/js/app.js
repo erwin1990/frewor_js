27bf45af07471cc8d537cfd4f2d674a5f394f886
erwin <camperwin@hotmail.com>
2019-03-29 15:53:34 -0300

    ddd

--- evaluacion_final/js/app.js
+++ evaluacion_final/js/app.js
@@ -17,9 +17,24 @@ function blanco(elemento) {
   )
 }
 
+var car1 = "<div class='caramelo'><img src='image/1.png'/></div>"
+var car2 = "<div class='caramelo'><img src='image/2.png'/></div>"
+var car3 = "<div class='caramelo'><img src='image/3.png'/></div>"
+var car3 = "<div class='caramelo'><img src='image/4.png'/></div>"
+
+$(".col-1").attr("src",function(){
+  var numero = Math.floor((Math.random() * 3) +1);
+  var ruta = "image/"+numero+".png";
+  return ruta;
+});
+
 $( function() {
 
   amarrillo($(".main-titulo"));
   blanco($(".main-titulo"));
+
+
+  $(".col-1").append(car1)
+
 
 });
